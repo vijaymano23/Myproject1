e1056fd462f47be2ff213882f335dac5e8ac0dbe
vijaymano23 <vijaymanovenselv@gmail.com>
2026-04-06 11:37:23 +0530

    adding two new files

--- dictionaries.py
+++ dictionaries.py
@@ -1,6 +1,10 @@
 dic={'name':'vijay','age':29,'city':'chennai'}
-print(dic)
-print(dic['city'])
-print(dic.get('name'))
-dic['name']='mano'
-print(dic)
+# print(dic)
+# print(dic['city'])
+# print(dic.get('name'))
+# dic['name']='mano'
+# print(dic)
+vaal=dic.pop('age')
+print(vaal)
+key,value=dic.popitem();
+print({key},{value})
---
+++ set.py
@@ -0,0 +1,6 @@
+a={1,22,4,5,7}
+b={10,2,3,4,5}
+print(a|b) #union
+print(a&b) #intersection
+print(a-b) #difference
+print(a^b) #symmetric difference
